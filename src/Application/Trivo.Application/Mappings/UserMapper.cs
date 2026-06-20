937c3dcbcef94fa16d1f6beb6da78ed0e59a899c
WilmerDev <wilmerdelacruz.dev@gmail.com>
2026-06-20 14:05:34 -0400

    feat: add interest and skill mapping methods to UserMapper

--- src/Application/Trivo.Application/Mappings/UserMapper.cs
+++ src/Application/Trivo.Application/Mappings/UserMapper.cs
@@ -1,10 +1,13 @@
+using Trivo.Application.DTOs.Interests;
+using Trivo.Application.DTOs.Skills;
 using Trivo.Application.DTOs.User;
+using Trivo.Domain.Models;
 
 namespace Trivo.Application.Mappings;
 
 public static class UserMapper
 {
-    public static UserDto MapUserDto(Domain.Models.User user)
+    public static UserDto MapToUserDto(Domain.Models.User user)
     {
         return new UserDto(
             Id: user.Id,
@@ -12,5 +15,25 @@ public static class UserMapper
             LastName: user.LastName,
             ProfilePictureUrl: user.ProfilePicture
         );
+    }
+
+    public static List<InterestWithIdDto> MapToInterests(ICollection<UserInterest> userInterests)
+    {
+        return userInterests
+            .Where(ui => ui.Interest is not null)
+            .Select(ui => new InterestWithIdDto(
+                InterestId: ui.Interest!.Id,
+                Name: ui.Interest.Name ?? string.Empty))
+            .ToList() ?? [];
+    }
+
+    public static List<SkillWithIdDto> MapToSkills(ICollection<UserSkill> userSkills)
+    {
+        return userSkills?
+            .Where(us => us.Skill is not null)
+            .Select(us => new SkillWithIdDto(
+                SkillId: us.Skill!.SkillId ?? Guid.Empty,
+                Name: us.Skill.Name ?? string.Empty))
+            .ToList() ?? [];
     }
 }
