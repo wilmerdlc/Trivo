991b33f8f654160e07070e17d3b35d6cf40424e3
wilmerdlc <wilmerdelacruz.dev@gmail.com>
2026-09-22 22:40:50 -0400

    feat: remove BanUser command and related handler and validator

--- src/Application/Trivo.Application/Features/Administrator/Commands/BanUser/BanUserCommand.cs
+++
@@ -1,5 +0,0 @@
-using Trivo.Application.Abstractions.Messages;
-
-namespace Trivo.Application.Features.Administrator.Commands.BanUser;
-
-public sealed record BanUserCommand(Guid UserId) : ICommand<string>;
